9d793e57c39e989d210fcf17b934f7386ce6b28e
jeffamaxey <96902951+jeffamaxey@users.noreply.github.com>
2023-05-04 06:21:47 -0400

    Update decorators.py

--- src/decorators.py
+++ src/decorators.py
@@ -1,4 +1,23 @@
+def add_method(object, method, name=None):
+    """The following function will add any function to an instance in a cycle-free way by creating a specially modified class object and changing the instance’s class to it:"""
+    if name is None: name = method.func_name
+    class newclass(object._ _class_ _):
+        pass
+    setattr(newclass, name, method)
+    object._ _class_ _ = newclass
 
+def extends(cls):
+    """Add method to class.
+    Method should have at least one argument which will be used as reference to object (i.e. self).
+    :param cls: Class to which method should be added
+    :return:
+    """
+
+    def decorator(func):
+        setattr(cls, func.__name__, func)
+        return func
+
+    return decorator
 
 def define_method(cls):
     """
